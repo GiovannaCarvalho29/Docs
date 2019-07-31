ccccc1a6e2df7edb7f606f77df25cbeaf0e98f83
GiovannaCarvalho29 <53492880+GiovannaCarvalho29@users.noreply.github.com>
2019-07-31 16:48:40 -0300

    Jo Ken Pô

---
+++ joke.py
@@ -0,0 +1,30 @@
+import time
+import random
+import os
+
+lista = ["pedra", "Papel", "Tesoura"]
+computador = int[0,2]
+Pergunta = int(input("""Escolha uma opção:
+
+                     [0]Pedra
+                     [1]Papel
+                     [2]Tesoura
+
+                     Digite sua opção:"""))
+
+print("Jokenpô!!!")
+time.sleep(1)
+
+print("O Computador escolheu:"
+print("O Jogador escolheu: "
+
+if computador == 0:
+    if Pergunta == 0:
+        print("Empate!")
+    elif Pergunta == 1:
+        print("Jogador Ganhou!")
+    elif Pergunta == 2:
+        print("Jogador Perdeu")
+    else:
+        print("Jogada invalida!")
+                                    
